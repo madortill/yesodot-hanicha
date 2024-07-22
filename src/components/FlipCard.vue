580dcea5fc691d431f2bbd8cdc1078fa1577db7f
madortill <mador.till@gmail.com>
2024-07-22 15:39:39 +0300

    moved to public

--- src/components/FlipCard.vue
+++ src/components/FlipCard.vue
@@ -27,10 +27,10 @@
     props: ['folder', 'frontName', 'backName'],
     computed: {
       srcFront() {
-        return `url(${document.location.href}/src/assets/media/${this.folder}/${this.frontName}.png`;
+        return `url(${document.location.href}/introduction/${this.folder}/${this.frontName}.png`;
       },
       srcBack() {
-        return `url(${document.location.href}/src/assets/media/${this.folder}/${this.backName}.png`;
+        return `url(${document.location.href}/introduction/${this.folder}/${this.backName}.png`;
       }
     },
   }
